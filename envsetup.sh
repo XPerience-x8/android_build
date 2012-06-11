951919c658daa9f949e87443285b1b0342f8bb47
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-06-11 12:31:54 +0300

    Strip cm- from output zipfile
    Change-Id: Ia1277e7c84382c4ca032545c86dbef8a25136363

--- envsetup.sh
+++ envsetup.sh
@@ -646,7 +646,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=`sed -n -e'/ro\.cm\.version/s/.*=//p' $OUT/system/build.prop`
-        ZIPFILE=cm-$MODVERSION.zip
+        ZIPFILE=$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
