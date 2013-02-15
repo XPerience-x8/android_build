ef7ffc0db9853526a7dc655ee1b53591931db4e6
nobodyAtall <tsogkas@ceid.upatras.gr>
2013-02-15 23:41:49 +0200

    Use armv6 build flags
    Change-Id: I99d17d2ee8b5a0d8875696f5f64e514768a73f09

--- core/combo/arch/arm/armv6-vfp.mk
+++ core/combo/arch/arm/armv6-vfp.mk
@@ -9,10 +9,19 @@ ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_VFP               := true
 
+ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
+TARGET_ARCH_VARIANT_FPU         := vfp
+endif
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
+TARGET_ARCH_VARIANT_CPU         := arm1136jf-s
+endif
+
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
 arch_variant_cflags := \
+    -mfloat-abi=softfp \
+    -mfpu=$(TARGET_ARCH_VARIANT_FPU) \
     -march=armv6k \
     -mtune=arm1136j-s  \
     -D__ARM_ARCH_6__ \
